fa011f3a7f8aab20d6fe4526be3da6e23301781b
Renan Oliveira <renan.oliveira.webdesigner@gmail.com>
2021-08-07 11:37:10 -0300

    Nested hooks example

--- app/spec/nested_hooks_spec.rb
+++ app/spec/nested_hooks_spec.rb
@@ -1,10 +1,26 @@
-# Task for the course
+class Message
+  attr_accessor :text
 
-# The order of letters:
+  def initialize(text = 'Some message')
+    @text = text
+  end
+end
 
-'A'
-'C'
-'D'
-'C'
-'D'
-'B'
+RSpec.describe Message do
+  before(:context) { @learning = 'I am learning' }
+  before { @object = 'Rspec' }
+
+  it 'expect message return' do
+    subject.text = "#{@learning} #{@object}"
+    expect(subject.text).to eq('I am learning Rspec')
+  end
+
+  context 'with rspec the first word in text' do
+    before { @goal = 'is a framework that I will learning' }
+
+    it 'expect other message return' do
+      subject.text = "#{@object} #{@goal}"
+      expect(subject.text).to eq('Rspec is a framework that I will learning')
+    end
+  end
+end
